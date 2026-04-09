aba7f4184469d9464cedc500cafdad81e761aa4d
kiruialvin <kiruialvin111@gmail.com>
2026-04-09 13:54:10 +0300

    i have created a splash activity

---
+++ app/src/main/java/com/Alvin/chui_wellnessapp/splash.kt
@@ -0,0 +1,43 @@
+package com.Alvin.chui_wellnessapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
+import android.os.Looper
+import kotlin.reflect.KClass
+
+
+
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent =Intent (this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },2000)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
